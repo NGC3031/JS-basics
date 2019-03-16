b23f7b9d8f1ed3a801f746ebe72b4a8c13c1e728
NGC3031 <scott.d@ieee.org>
2019-03-17 08:25:22 +1100

    js scripts

---
+++ fns-snippet-fn-expressions.js
@@ -0,0 +1,7 @@
+// function declaration through function expressions
+// we can assign the variable multiply the value of the anonymous function
+var multiply = function (a, b) {
+    return a * b;
+}
+c = multiply(3, 2);
+console.log(c); // 6
