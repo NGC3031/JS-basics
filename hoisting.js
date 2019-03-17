eae128ee80af245e543477672cedfbc682c3e77c
NGC3031 <scott.d@ieee.org>
2019-03-18 10:34:50 +1100

    added

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+// Javascript hoisting demonstration
+
+a = 2; //we can assign 2 to a
+console.log(a);
+
+var a; // declaration is moved to the top by the interpreter before execution
+
+
+me();
+
+// the function me is moved up also even though it is declared after the call
+// compare that to say c/c++
+function me() {
+    console.log('Hello me');
+}
