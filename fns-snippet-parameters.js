7fda3b8c676831bcef701a9860cf0207dfc3b2d4
NGC3031 <scott.d@ieee.org>
2019-03-18 10:57:41 +1100

    added

--- fns-snippet-parameters.js
+++ fns-snippet-parameters.js
@@ -1,6 +1,12 @@
-// basic function declaration
-function multiply(a, b) {
-    return a * b;
-}
-c = multiply(3, 2);
-console.log(c); // 6
+// basic function parameters
+// Javascript using multiple parameters
+
+var sum = function () {
+    var i, total = 0;
+    //get the total of the arguments
+    for (i = 0; i < arguments.length; i += 1) {
+        total += arguments[i];
+    }
+    return total;
+};
+console.log(sum(1, 2, 3, 4, 5, 6, 7, 8, 9));
