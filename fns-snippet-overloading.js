b78e059986c3b62518cd07e68cbf9e2616091329
NGC3031 <scott.d@ieee.org>
2019-04-06 00:32:37 +1100

    fns-snippet-overloading.js

---
+++ fns-snippet-overloading.js
@@ -0,0 +1,24 @@
+// Simulating overloading
+// Javascript functions can accept any number of parameters through the
+// arguments object which also means they have no function signatures
+// Work arounds for function overloading
+
+function simOverload(msg) {
+    if (arguments.length === 0) {
+        msg = 'Default';
+    }
+    if (arguments.length === 1) {
+        msg = 'One cat.';
+    }
+    if (arguments.length === 2) {
+        msg = 'And lots of mice.';
+    }
+    if (arguments.length === 3) {
+        msg = 'And four Owls';
+    }
+    return console.log(msg);
+}
+simOverload();
+simOverload(1);
+simOverload(1, 2);
+simOverload(1, 2, 3);
