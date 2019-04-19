4c00989dddf33837fcf00f53b83e189563d0e339
NGC3031 <scott.d@ieee.org>
2019-04-19 11:03:58 +1000

    update

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+//example of basic map function
+
 function map(array) {
     return arr.map(Math.sqrt);
 }
@@ -5,3 +7,13 @@ function map(array) {
 arr = [3, 4, 5, 6, 7];
 
 console.log(map(arr));
+
+//more complex mapping
+
+function wordMap(array) {
+    return arrWords.map(s => 'Police' + s);
+}
+
+arrWords = ['car', 'truck', 'motorcycle'];
+
+console.log(wordMap(arrWords));
